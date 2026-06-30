5383c7eeeae4b50a2fdb8200f95d904163678d6e
Arellano Cruz Francisco <masterchif321289@gmail.com>
2026-06-29 22:24:07 -0600

    Fix case-sensitive imports para Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import "./styles/Global.css";
-import "./styles/Forms.css";
-import "./styles/Table.css";
+import "./Styles/Global.css";
+import "./Styles/Forms.css";
+import "./Styles/Table.css";
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
